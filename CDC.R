d6cf4218e2aec687a2d0f661e83627d82abc1754
syed-shah-1 <srshah15@unc.edu>
2024-09-28 10:47:40 -0400

    read file in

--- CDC.R
+++ CDC.R
@@ -1,4 +1,11 @@
 library(tidyverse)
 
 #healthdata <- read.csv("/Users/unitig/cdc2024/Health_Science_Dataset.csv", header = TRUE)
-healthdata <- read.csv("C:/Users/Raeds/Documents/cdc2024/Health_Science_Dataset.csv", header = TRUE)
+healthdata <- read.csv("C:/Users/Raeds/Documents/cdc2024/Health_Science_Dataset.csv", skip = 1, header = FALSE)
+
+colnames(healthdata) <- healthdata[1, ]
+
+# Step 3: Remove the now redundant first row (which was used for headers)
+healthdata <- healthdata[-1, ]
+
+View(healthdata)
