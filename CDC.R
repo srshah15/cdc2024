87a156bbe86e68c98d5f6174d802d50e4223c546
syed-shah-1 <srshah15@unc.edu>
2024-09-28 10:25:54 -0400

    created r file

---
+++ CDC.R
@@ -0,0 +1,4 @@
+library(tidyverse)
+
+#healthdata <- read.csv("/Users/unitig/cdc2024/Health_Science_Dataset.csv", header = TRUE)
+healthdata <- read.csv("C:/Users/Raeds/Documents/cdc2024/Health_Science_Dataset.csv", header = TRUE)
